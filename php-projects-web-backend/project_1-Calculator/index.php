86e1389cd2814cc234869fd0bfb6493e6a6b625b
Mohammad Sharif <ermohdsharif@gmail.com>
2023-03-21 18:44:42 +0530

    I am project

--- php-projects-web-backend/project_1-Calculator/index.php
+++ php-projects-web-backend/project_1-Calculator/index.php
@@ -1,10 +1,37 @@
 <html>
-    <head>
-        <title>basic area calcular in php</title>
-    </head>
 
-    <body>
-        <div class="container"></div>
-    </body>
+<head>
+    <title>basic area calcular in php</title>
+</head>
+
+<body>
+    <div class="container">
+        <form action="" method="POST">
+            <div class="select">
+                <label for="shape">I want to calculate area of:</label>
+                <select name="shape" id="shape">
+                    <option value="circle">circle</option>
+                    <option value="square">square</option>
+                    <option value="rectangle">rectangle</option>
+                    <option value="sphere">sphere</option>
+                </select>
+            </div>
+            <input type="submit" name="submit" value="Submit">
+        </form>
+    </div>
+
+    <!-- PHP code -->
+    <p>
+        <?php
+            if(isset($_POST['submit'])){
+                $shape = $_POST['shape'];     
+                echo $shape;      
+            }
+            
+        ?>
+    </p>
+</body>
+
+
 
 </html>
