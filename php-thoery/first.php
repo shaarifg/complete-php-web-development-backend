da6e379369d9fdf5b58657e846f055b6188a3e77
Mohammad Sharif <ermohdsharif@gmail.com>
2023-03-21 15:12:54 +0530

    first commit

---
+++ php-thoery/first.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Hello My name is Sharif";
+
+// if there is only php code in the file then we don't need closing tag for php
+
+# Variable
+
+$_myName = 'Sharif';
+$value = 10.5;
+echo $value;
